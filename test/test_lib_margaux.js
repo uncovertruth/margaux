afa0a9dfabe4265a4d2092441d21949ba8c3fc16
taka-sho <takasho530000@gmail.com>
2017-01-23 17:24:24 +0900

    chore(test_lib_margaux) add tests : create, setCookie, and deleteCookie

--- test/test_lib_margaux.js
+++ test/test_lib_margaux.js
@@ -4,6 +4,7 @@ import _ from 'lodash'
 import assert from 'assert'
 import co from 'co'
 import promisify from 'es6-promisify'
+import errors from 'common-errors'
 
 describe('lib/margaux', () => {
   const utils = require('../lib/utils')
@@ -33,6 +34,67 @@ describe('lib/margaux', () => {
         assert.ok(err === null)
       })
     })
+  })
+
+  it('should not create without host and port', function (done) {
+    margaux.create('', 0, (err) => {
+      if (!err) {
+        assert(false)
+      }
+      done()
+    })
+  })
+
+  it('should not set cookie without cookie name', function (done) {
+    const chrome = createTmpServer('', {})
+    const cookieOpts = {
+      cookieName: '',
+      value: 12345
+    }
+    margaux.setCookie(chrome, cookieOpts, (err) => {
+      assert(err instanceof errors.ArgumentNullError)
+      assert(err.message === 'Missing argument: cookieName')
+    })
+    done()
+  })
+
+  it('should not set cookie without value name', function (done) {
+    const chrome = createTmpServer('', {})
+    const cookieOpts = {
+      cookieName: 'test',
+      value: ''
+    }
+    margaux.setCookie(chrome, cookieOpts, (err) => {
+      assert(err instanceof errors.ArgumentNullError)
+      assert(err.message === 'Missing argument: value')
+    })
+    done()
+  })
+
+  it('should not delete cookie without cookieName', function (done) {
+    const chrome = createTmpServer('', {})
+    const cookieOpts = {
+      cookieName: '',
+      url: 'http://localhost/'
+    }
+    margaux.deleteCookie(chrome, cookieOpts, (err) => {
+      assert(err instanceof errors.ArgumentNullError)
+      assert(err.message === 'Missing argument: cookieName')
+    })
+    done()
+  })
+
+  it('should not delete cookie without url', function (done) {
+    const chrome = createTmpServer('', {})
+    const cookieOpts = {
+      cookieName: 'test',
+      url: ''
+    }
+    margaux.deleteCookie(chrome, cookieOpts, (err) => {
+      assert(err instanceof errors.ArgumentNullError)
+      assert(err.message === 'Missing argument: url')
+    })
+    done()
   })
 
   it('(get|set|delete)Cookies', function (done) {
