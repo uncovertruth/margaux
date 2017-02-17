87a59a84028c9d4eeecfec20b0f694b501429be9
Keisuke Kan <kan@uncovertruth.co.jp>
2017-02-17 11:17:32 +0900

    fix(src) add type

--- src/const.js
+++ src/const.js
@@ -1,27 +1,18 @@
 /* @flow */
 'use strict'
 
-const storeDir = process.env.NODE_WEBSNAPSHOT_STORE_DIR ||
-  '/tmp/_margaux'
-
-const tabTimeout = process.env.NODE_NEO_PAGE_CACHE_TAB_TIMEOUT ||
-  30 * 1000
+export const CHROME_CHECK_URL: ?string = process.env.NODE_WEBSNAPHOT_CHECK_TEXT
+export const CLOSE_TAB_TIMEOUT: number = parseInt(process.env.NODE_NEO_PAGE_CACHE_TAB_TIMEOUT, 10) || 30 * 1000
+export const MOUNT_CHECK_CONTENT: string = 'OK'
+export const MOUNT_CHECK_FILE: string = 'margaux_check.txt'
+export const RAVEN_DSN: ?string = process.env.RAVEN_DSN
+export const REMOTE_DEBUGGING_PORTS_START: number = 9221
+export const WEBSNAPSHOT_STORE_DIR: string = process.env.NODE_WEBSNAPSHOT_STORE_DIR || '/tmp/_margaux'
 
 const numBrowser: any = process.env.NODE_WEBSNAPSHOT_NUM_BROWSER || 4
-
-const REMOTE_DEBUGGING_PORTS_START = 9221
-
-const debuggingPorts = []
-for (let i = 0; i < numBrowser; i++) {
+const debuggingPorts: number[] = []
+for (let i: number = 0; i < numBrowser; i++) {
   debuggingPorts.push(REMOTE_DEBUGGING_PORTS_START + i)
 }
 
-module.exports = {
-  REMOTE_DEBUGGING_PORTS: debuggingPorts,
-  WEBSNAPSHOT_STORE_DIR: storeDir,
-  CLOSE_TAB_TIMEOUT: tabTimeout,
-  MOUNT_CHECK_FILE: 'margaux_check.txt',
-  MOUNT_CHECK_CONTENT: 'OK',
-  RAVEN_DSN: process.env.RAVEN_DSN,
-  CHROME_CHECK_URL: process.env.NODE_WEBSNAPHOT_CHECK_TEXT
-}
+export const REMOTE_DEBUGGING_PORTSL: number[] = debuggingPorts
