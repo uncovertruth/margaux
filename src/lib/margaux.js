1859b9d6725cf5e505ccdce0a296790a37a4506c
taka-sho <takasho530000@gmail.com>
2017-04-28 18:42:42 +0900

    chore(lib_margaux) return error when arguments don't exist

--- src/lib/margaux.js
+++ src/lib/margaux.js
@@ -272,6 +272,9 @@ export function forceCharset (client: CDP, callback: (err: ?Error) => void) {
 
 export function close (client: CDP, cb: (err: ?Error, res: string) => void) {
   const {host, port, tab} = client
+  if (!host || !port || !tab) {
+    return error(new Error(`couldn't get any info. host: ${host}, port: ${port}, tab: ${tab}`))
+  }
   CDP.Close({
     host: host,
     port: port,
