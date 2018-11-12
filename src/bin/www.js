0a905019569fea1d088dc9097c4e889f68829b0a
makoto tsuyuki <mtsuyuki@gmail.com>
2018-11-12 16:08:20 +0900

    use temporary versioned inliner (#247)
    * use temporary versioned inliner
    
    * change flow version
    
    * make cache off
    
    * try to use nyc 13.1.0 to 13.0.1
    
    * Revert "try to use nyc 13.1.0 to 13.0.1"
    
    This reverts commit f0f7e5ae2e05c90d68e3d21e3d1691f23c90928c.
    
    * try to use eslint 5.7.0 to 5.6.1
    
    * try to delete yarn.lock
    
    * regenerate yarn.lock
    
    * update eslint
    
    * fix lint
    
    * Revert "make cache off"
    
    This reverts commit 52fe7942192db0fa8adec82fcfadf81df1febe0c.

--- src/bin/www.js
+++ src/bin/www.js
@@ -54,18 +54,18 @@ function onError (error) {
     throw error
   }
 
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges')
       process.exit(1)
+    /* falls through */
     case 'EADDRINUSE':
       console.error(bind + ' is already in use')
       process.exit(1)
+    /* falls through */
     default:
       throw error
   }
@@ -77,8 +77,6 @@ function onError (error) {
 
 function onListening () {
   const addr = server.address()
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
   debug('Listening on ' + bind)
 }
